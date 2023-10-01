a38af85a2b0c8be0a8937472c6a585f136dc5422
M Usman <129891430+mrusman1316@users.noreply.github.com>
2023-10-01 10:36:04 +0500

    Update zphisher.sh

--- zphisher.sh
+++ zphisher.sh
@@ -204,15 +204,10 @@ check_status() {
 banner() {
 	cat <<- EOF
 		${ORANGE}
-		${ORANGE} ______      _     _     _               
-		${ORANGE}|___  /     | |   (_)   | |              
-		${ORANGE}   / / _ __ | |__  _ ___| |__   ___ _ __ 
-		${ORANGE}  / / | '_ \| '_ \| / __| '_ \ / _ \ '__|
-		${ORANGE} / /__| |_) | | | | \__ \ | | |  __/ |   
-		${ORANGE}/_____| .__/|_| |_|_|___/_| |_|\___|_|   
-		${ORANGE}      | |                                
-		${ORANGE}      |_|                ${RED}Version : ${__version__}
-
+                ${ORANGE}╔╦╗╔══╗╔═╦═╗╔══╗╔═╦╗
+                ${ORANGE}║║║║══╣║║║║║║╔╗║║║║║
+                ${ORANGE}║║║╠══║║║║║║║╠╣║║║║║
+                ${ORANGE}╚═╝╚══╝╚╩═╩╝╚╝╚╝╚╩═╝${RED}Version : ${__version__}
 		${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by Usman Ser (Usman Ser)${WHITE}
 	EOF
 }
